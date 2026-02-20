73d25730f320aac7ddd7e51221395c126066a5c2
Cococry <98212066+cococry@users.noreply.github.com>
2026-02-20 09:09:36 +0100

    CFG functionality abstracted into cfg.h/.c

---
+++ src/cfg.h
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "ir.h"
+
+struct BasicBlock {
+
+  struct BasicBlock** predecessors, **successors;
+  size_t predecessors_n, successors_n;
+  size_t predecessors_cap, successors_cap;
+
+  int64_t label;
+
+  size_t id;
+
+  size_t func_idx;
+
+  size_t begin, end;
+};
+
+
+int8_t cfgbuild(const struct IRFunction* func, struct BasicBlock** o_blocks, size_t* o_blocks_n);
+
+void cfgprint(const struct BasicBlock *blocks, const size_t blocks_n);
